8c9ded5b4e760fd6455eaff420e26cdeb4cce390
Weslei Possidonio <weslwifranca123@gmail.com>
2026-09-22 14:42:34 -0300

    update link api

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://api-cartorio.vercel.app/',
   withCredentials: true,
 })
 
